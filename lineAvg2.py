89d0dbd31c5a8b896737ef2d7fedc1e1b3d3fc04
Kremona1924 <161258017+Kremona1924@users.noreply.github.com>
2024-02-26 10:47:52 +0100

    Update lineAvg2.py
    Remove own paths

--- lineAvg2.py
+++ lineAvg2.py
@@ -23,11 +23,9 @@ def get_score_per_line(filename):
     
     return scores_per_line
 
-# Path to the directory containing the results files
-results_directory = r"C:\Users\31613\OneDrive\Documenten\Artificial Intelligence\Semester 2\Natural Computing\Assignment 3\results_r2to6"
+results_directory = r"path\results_r2to6"
 output_directory = results_directory  
 
-# List of file bases and r values
 file_bases = ['results_0', 'results_1']
 r_values = ['r2', 'r3', 'r4', 'r5', 'r6']
 
@@ -36,11 +34,9 @@ for base in file_bases:
     for r in r_values:
         input_filename = os.path.join(results_directory, f"{base}_{r}.txt")
         output_filename = os.path.join(output_directory, f"average_{base}_{r}.txt")
-        
-        # Calculate average scores
+
         average_scores = get_score_per_line(input_filename)
         
-        # Write average scores to file
         with open(output_filename, 'w') as f:
             for avg in average_scores:
                 f.write(str(avg) + '\n')
