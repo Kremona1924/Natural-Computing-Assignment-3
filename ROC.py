5d0e7503262ec74b11d578cfb4b49d6ba9620bfa
Kremona1924 <161258017+Kremona1924@users.noreply.github.com>
2024-02-26 10:50:10 +0100

    Update ROC.py
    remove own paths

--- ROC.py
+++ ROC.py
@@ -10,8 +10,8 @@ langs = ['english', 'tagalog', 'xhosa', 'middle-english', 'hiligaynon', 'plautdi
 for lang in langs:
     auc = []
     for i in range(1,10):
-        english_scores = np.loadtxt(f'C:/Users/31613/OneDrive/Documenten/Artificial Intelligence/Semester 2/Natural Computing/Assignment 3/string_length_tests/string_length_tests/test_scores_english_{i}.txt')
-        scores = np.loadtxt(f'C:/Users/31613/OneDrive/Documenten/Artificial Intelligence/Semester 2/Natural Computing/Assignment 3/string_length_tests/string_length_tests/test_scores_{lang}_{i}.txt')
+        english_scores = np.loadtxt(f'path/string_length_tests/test_scores_english_{i}.txt')
+        scores = np.loadtxt(f'C:/path/string_length_tests/test_scores_{lang}_{i}.txt')
         labels = np.append(np.zeros(len(english_scores)), np.ones(len(scores)))
         data = np.append(english_scores, scores)
 
